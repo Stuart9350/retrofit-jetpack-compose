71c0eeeb71bc3821098023f09e2636c69a595974
Jatin Kumar <jk8285971003@gmail.com>
2023-05-03 16:14:50 +0530

    final commit

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,14 +31,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DataList(dataList = dataViewModel.dataListResponse)
-                    dataViewModel.getDataList()
+                    Column() {
+                        Id()
+                        DataList(dataList = dataViewModel.dataListResponse)
+                        dataViewModel.getDataList()
+                    }
                 }
             }
         }
     }
 }
 
+@Composable
+fun Id(){
+    Text(text = "Retrofit")
+}
 
 @Composable
 fun DataList(dataList: List<DataList>){
